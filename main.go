b85a393c389849205421cd04bab9257d44d11a8f
Simoncao <13671707129@126.com>
2021-08-24 18:00:47 +0800

    [feat]add grep to handler

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
 )
 
-var (
-	logDir string = "/data/webActivity/"
-)
+var logDir string = "/data/webActivity/"
 
 func main() {
 	http.HandleFunc("/getTimes", getTimes)
@@ -20,5 +19,13 @@ func getTimes(w http.ResponseWriter, r *http.Request) {
 	uid := r.Form["uid"]
 	sid := r.Form["sid"]
 	fmt.Println(uid[0], sid[0])
+	cmd := fmt.Sprintf("grep %s %s/%s/* | grep lottery | wc -l", uid[0], sid[0], logDir)
+	out, err := exec.Command("/bin/bash", "-c", cmd).Output()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(string(out))
+
 	fmt.Fprintf(w, "hello")
 }
