f3e807303861a28f083847c8549d34f69f16cf53
Simoncao <13671707129@126.com>
2021-08-24 17:49:35 +0800

    [feat]init http server

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"net/http"
 )
 
+var (
+	logDir string = "/data/webActivity/"
+)
+
 func main() {
 	http.HandleFunc("/getTimes", getTimes)
 	log.Panic(http.ListenAndServe(":8080", nil))
@@ -15,6 +19,6 @@ func getTimes(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	uid := r.Form["uid"]
 	sid := r.Form["sid"]
-	fmt.Println(uid, sid)
+	fmt.Println(uid[0], sid[0])
 	fmt.Fprintf(w, "hello")
 }
