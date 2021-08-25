fa42a9b036846ff68acf3840802d966e2bd4cb3e
Simoncao <13671707129@126.com>
2021-08-25 17:26:14 +0800

    bak

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func getTimes(w http.ResponseWriter, r *http.Request) {
 	uid := r.Form["uid"]
 	sid := r.Form["sid"]
 	fmt.Println(uid[0], sid[0])
+
 	cmd := fmt.Sprintf("LC_ALL=C fgrep lottery %s/%s/* | LC_ALL=C fgrep '{\"jid\":\"109'| LC_ALL=C fgrep %s | wc -l", logDir, sid[0], uid[0])
 	out, err := exec.Command("/bin/bash", "-c", cmd).Output()
 
