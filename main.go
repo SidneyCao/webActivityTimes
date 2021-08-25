50d805787678ee2eaf61762eed3108ac3790163b
Simoncao <13671707129@126.com>
2021-08-25 12:14:51 +0800

    [test]grep func

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func getTimes(w http.ResponseWriter, r *http.Request) {
 	uid := r.Form["uid"]
 	sid := r.Form["sid"]
 	fmt.Println(uid[0], sid[0])
-	cmd := fmt.Sprintf("grep %s %s/%s/* | grep lottery | wc -l", uid[0], logDir, sid[0])
+	cmd := fmt.Sprintf("grep lottery %s/%s/* | grep %s | wc -l", logDir, sid[0], uid[0])
 	out, err := exec.Command("/bin/bash", "-c", cmd).Output()
 	if err != nil {
 		log.Panic(err)
